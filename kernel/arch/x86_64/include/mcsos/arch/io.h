305e3e102c69445b4e94aac0ddae6986975acc6e
Mikail <Mikailkhairurahman@gmail.com>
2026-05-24 00:21:31 +0700

    M5: add x86_64 io abstraction

--- kernel/arch/x86_64/include/mcsos/arch/io.h
+++ kernel/arch/x86_64/include/mcsos/arch/io.h
@@ -3,30 +3,58 @@
 
 #include <stdint.h>
 
-static inline void outb(uint16_t port, uint8_t value) {
-    __asm__ volatile (
+static inline void outb(
+    uint16_t port,
+    uint8_t value
+)
+{
+    __asm__ volatile(
         "outb %0, %1"
         :
         : "a"(value), "Nd"(port)
-        : "memory"
     );
 }
 
-static inline uint8_t inb(uint16_t port) {
+static inline uint8_t inb(
+    uint16_t port
+)
+{
     uint8_t value;
 
-    __asm__ volatile (
+    __asm__ volatile(
         "inb %1, %0"
         : "=a"(value)
         : "Nd"(port)
-        : "memory"
     );
 
     return value;
 }
 
-static inline void io_wait(void) {
-    outb(0x80u, 0u);
+static inline void cli(void)
+{
+    __asm__ volatile("cli");
+}
+
+static inline void sti(void)
+{
+    __asm__ volatile("sti");
+}
+
+static inline void hlt(void)
+{
+    __asm__ volatile("hlt");
+}
+
+static inline uint16_t read_cs(void)
+{
+    uint16_t cs;
+
+    __asm__ volatile(
+        "mov %%cs, %0"
+        : "=r"(cs)
+    );
+
+    return cs;
 }
 
 #endif
